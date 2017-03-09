b64b72e54726cb209eb1f2632f7dc917a039975f
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-09 08:56:26 +0000

    Change datamapper setup to only be in dm config

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,6 @@
 class Link
   include DataMapper::Resource
 
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
   property :id, Serial
   property :title, Text
   property :url, Text
--- config.ru
+++ config.ru
@@ -2,6 +2,4 @@ require 'rubygems'
 require './app'
 require 'data_mapper'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
 run App
