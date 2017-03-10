7df502fa56fc8728e662fcf20fa5bc2002883746
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-10 16:47:38 +0000

    Validates email address

--- spec/features/user_entry_spec.rb
+++ spec/features/user_entry_spec.rb
@@ -11,4 +11,12 @@ feature "User entry" do
     expect(current_path).to eq('/')
     expect(page).to have_content("Password and confirmation password do not match")
   end
+
+  scenario "requires email address to be filled in for sign up" do
+    expect{sign_up(email: nil)}.not_to change(User, :count)
+  end
+
+  scenario "requires email address to be valid format (realemail@address.com)" do
+    expect{sign_up(email: "someone@somewhere")}.not_to change(User, :count)
+  end
 end
